df0bee6fa82e190be8654f52665ccaa7475bcc79
Lesha Zhoglik <programm.lesha@mail.ru>
2020-04-18 14:10:06 +0300

    Add list records for only user

--- src/app/pages/home/my-records/my-records.component.ts
+++ src/app/pages/home/my-records/my-records.component.ts
@@ -8,9 +8,21 @@ import {DoctorsService} from '../../../shared/services/doctors.service';
 })
 export class MyRecordsComponent implements OnInit {
 
+  name: string = '';
+
   constructor(public doctorsService: DoctorsService) {}
 
   ngOnInit(): void {
     this.doctorsService.getDoctors();
+
+    this.name = localStorage.getItem('user_nameFirst') + ' ' + localStorage.getItem('user_nameLast');
+  }
+
+  removeRecord(doctor, docKey, day, time) {
+    const rem = confirm(`Delete record ${doctor} (${day} - ${time}) ?`);
+
+    if (rem) {
+      this.doctorsService.removeRecord(doctor, docKey, day, time).subscribe(() => this.doctorsService.getDoctors(), err => console.error(err));
+    }
   }
 }
